9b213e70add386084360ed604cf51616db82f660
enailecr <enaile.crebello@gmail.com>
2018-09-13 10:19:49 -0300

    Correção bugs

--- Inventario/Inventario/urls.py
+++ Inventario/Inventario/urls.py
@@ -19,11 +19,11 @@ from .views import home
 
 urlpatterns = [
     path(r'', home),
-    path(r'componentes/', include('componentes.urls'))
-    path(r'contatos/', include('contatos.urls'))
-    path(r'dispositivos/', include('dispositivos.urls'))
-    path(r'instituicoes/', include('instituicoes.urls'))
-    path(r'projetos/', include('projetos.urls'))
-    path(r'usuarios/', include('usuarios.urls'))
+#    path(r'componentes/', include('componentes.urls')),
+#    path(r'contatos/', include('contatos.urls')),
+#    path(r'dispositivos/', include('dispositivos.urls')),
+#    path(r'instituicoes/', include('instituicoes.urls')),
+#    path(r'projetos/', include('projetos.urls')),
+    path(r'usuarios/', include('usuarios.urls')),
     path('admin/', admin.site.urls),
 ]
--- Inventario/usuarios/urls.py
+++ Inventario/usuarios/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import home
+from django.urls import path
+from .views import login, add
 
 urlpatterns = [
-    path(r'login/', login))
-    path(r'add/',add)
+    path(r'login/', login),
+    path(r'add/',add),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
