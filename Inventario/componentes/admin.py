c84a056d48812b7dadcae3a77c15ee02938f2faf
CAM <enaile.crebello@gmail.com>
2018-09-27 10:32:54 -0300

    add componentes ao admin para futuramente definir quais são as permissões de cada usuario

--- Inventario/componentes/admin.py
+++ Inventario/componentes/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import TipoComponente
+from .models import TipoComponente, Componente
 
 admin.site.register(TipoComponente)
+admin.site.register(Componente)
--- Inventario/dispositivos/admin.py
+++ Inventario/dispositivos/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import StatusDispositivos
+from .models import StatusDispositivos, Anotacao, Dispositivo
 
 admin.site.register(StatusDispositivos)
+admin.site.register(Dispositivo)
+admin.site.register(Anotacao)
