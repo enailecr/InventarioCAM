f505dda17eace97a2c07a355d8f951b8043a40c7
CAM <enaile.crebello@gmail.com>
2018-10-01 09:55:15 -0300

    tabela com coluna para edição e exclusão

--- Inventario/projetos/tables.py
+++ Inventario/projetos/tables.py
@@ -1,7 +1,13 @@
 import django_tables2 as tables
 from .models import Projeto
+from django.utils.html import format_html
+
+class ButtonColumn(tables.Column):
+    def render(self):
+        return format_html('<input type="button" class="edit-button">')
 
 class ProjetoTable(tables.Table):
     class Meta:
         model = Projeto
-        fields = ('sigla', 'nome')
+        editar = ButtonColumn()
+        fields = ('sigla', 'nome', 'editar', 'excluir')
