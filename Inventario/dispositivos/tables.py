81cb9f0f4e298b7d2c11c30059cc86076f8c189e
simoesjonatas <jonatasimoes.js@gmail.com>
2018-10-04 14:51:30 -0300

    adicionando a merda do botao dentro da tabela

--- Inventario/dispositivos/tables.py
+++ Inventario/dispositivos/tables.py
@@ -1,10 +1,28 @@
 import django_tables2 as tables
 from .models import Dispositivo
 
+from django.db import models
+from django.utils import six
+from django.utils.html import escape, format_html
+
+from django_tables2.utils import AttributeDict, ucfirst
+
 class DispositivoTable(tables.Table):
+    excluir = tables.TemplateColumn(
+            '<form action="/dispositivos/dispositivo-remove/{{record.id}}/" method="post">{% csrf_token %}<input type="hidden" name="_method" value="Excluir"><button data-toggle="tooltip" title="Please note that deletion cannot be undone" type="submit" class="btn btn-danger btn-xs">Excluir</button></form>',
+        orderable=False,
+        verbose_name=''
+        )
+    editar = tables.TemplateColumn(
+            '<form action="/dispositivos/dispositivo-edita/{{record.id}}/" method="get">{% csrf_token %}<input type="hidden" name="_method" value="Editar"><button data-toggle="tooltip" title="Please note that deletion cannot be undone" type="submit" class="btn btn-danger btn-xs">Editar</button></form>',
+        orderable=False,
+        verbose_name=''
+        )
     class Meta:
-        model = Dispositivo
         fields = ('unidade.instituicao.sigla', 'unidade.sigla', 'projeto', 'componente', 'ip', 'core', 'web', 'ipvirtual', 'atualizado', 'status','editar','excluir')
+        atualizado  = tables.BooleanColumn('atualizado')
+        model = Dispositivo
+        template_name = 'django_tables2/bootstrap.html'
         labels = {
             'ipvirtual': ('Extra'),
         }
